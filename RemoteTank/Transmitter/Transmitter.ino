345fb6913506386b3550c3f50da6b33a296f611a
원호 이 <graysunday@outlook.kr>
2018-11-06 23:27:12 +0900

    2018-11-06 1st

---
+++ RemoteTank/Transmitter/Transmitter.ino
@@ -0,0 +1,36 @@
+﻿#include <RF24_config.h>
+#include <printf.h>
+#include <Joystick.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte addresses[6] = "77777";
+
+void setup() {
+	radio.begin();
+	radio.openWritingPipe(addresses);
+	radio.setPALevel(RF24_PA_MIN);
+	radio.stopListening();
+}
+
+void loop() {
+	
+	int xPin = 1; /* analog */
+	int yPin = 0; /* analog */
+	int bPin = 3; /* digital */
+
+	Joystick joystick(xPin, yPin, bPin); /* joystick pin No. */
+	
+	int Direction = joystick.getDirection();  /* 0: stop, 1:forward, 2:backward, 3:right, 4:left, 13, 14, 23, 24 */
+	int xSpeed = joystick.getX();
+	int ySpeed = joystick.getY();
+
+	radio.write(&Direction, sizeof(Direction));
+	radio.write(&xSpeed, sizeof(xSpeed));
+	radio.write(&ySpeed, sizeof(ySpeed));
+	const char text[] = "Hello World";
+	radio.write(&text, sizeof(text));
+
+}
---
+++ RemoteTank/receiver/receiver.ino
@@ -0,0 +1,45 @@
+﻿#include <RF24_config.h>
+#include <printf.h>
+#include <L298N.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(6, 10); // CE, CSN
+const byte addresses[6] = "77777";
+
+void setup() {
+
+	Serial.begin(9600);
+	radio.begin();
+	radio.openReadingPipe(0, addresses);
+	radio.setPALevel(RF24_PA_MIN);
+	radio.startListening();
+}
+
+void loop() {
+	
+	radio.startListening();
+
+	int ena = 5;  /* digital pwm  */
+	int in1 = 2;  /* digital      */
+	int in2 = 4;  /* digital      */
+	int in3 = 7;  /* digital      */
+	int in4 = 8;  /* digital      */
+	int enb = 9;  /* digital pwm  */
+
+	L298N driver(ena, in1, in2, in3, in4, enb);  /* L298N pin No. */
+		
+	if (radio.available()) {
+			int Direction = 0;
+			int xSpeed = 0;
+			int ySpeed = 0;
+			radio.read(&Direction, sizeof(Direction));
+			radio.read(&xSpeed, sizeof(xSpeed));
+			radio.read(&ySpeed, sizeof(ySpeed));
+			driver.dualWheelOutput(Direction, xSpeed, ySpeed);
+			
+			char text[32] = "";
+			radio.read(&text, sizeof(text));
+			Serial.println(text);
+}
